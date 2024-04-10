2075b7f364a38d851de8e44c030cf0642737461f
Jed-home <jedbez@mail.ru>
2024-04-10 23:34:05 +1000

    заданы пути запросов, подключен роутер добавлены ViewSet

--- pereval/views.py
+++ pereval/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from pereval.models import Passes, Images, Level, Coords, PassUser
+from pereval.serializers import PassSerializer, ImagesSerializer, LevelSerializer, CoordsSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = PassUser.objects.all()
+    serializer_class = UserSerializer
+
+
+class CoordsViewSet(viewsets.ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = CoordsSerializer
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+
+
+class PassesViewSet(viewsets.ModelViewSet):
+    queryset = Passes.objects.all()
+    serializer_class = PassSerializer
