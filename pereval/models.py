fa73b2dfc45a8636a3ec4f09d87d95347aad05a3
Jed-home <jedbez@mail.ru>
2024-04-10 23:19:40 +1000

    Первичная структура базы данных

--- pereval/models.py
+++ pereval/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Added(models.Model):
+    date_added = models.DateTimeField()
+    raw_data = models.JSONField()
+    images = models.JSONField()
+
+
+class Areas(models.Model):
+    id_parent = models.IntegerField(blank=False)
+    title = models.TextField()
+
+
+class Images(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(upload_to='images/')
+
+
+class Spr_Activities_Types(models.Model):
+    title = models.TextField()
